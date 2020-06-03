e69c7f2a4757b9999e5e23146ae698c7ec37b09a
Graham Otte <go@grahamotte.com>
2020-06-02 21:44:07 -0700

    Updates

---
+++ precache-manifest.a873167c5eb8ea1d88a2fc93923acec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5a0c10efb8f9bc068dfd6dd8b1459f",
+    "url": "/tssdne/index.html"
+  },
+  {
+    "revision": "a6cf4a70b27cc25e530a",
+    "url": "/tssdne/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "85aa47209314a257b6af",
+    "url": "/tssdne/static/js/2.288de776.chunk.js"
+  },
+  {
+    "revision": "fe5140e8f9e310d4360aa0e1d2676d41",
+    "url": "/tssdne/static/js/2.288de776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6cf4a70b27cc25e530a",
+    "url": "/tssdne/static/js/main.b592bf73.chunk.js"
+  },
+  {
+    "revision": "bed604e934f5d049c530",
+    "url": "/tssdne/static/js/runtime-main.67d9320e.js"
+  }
+]);
